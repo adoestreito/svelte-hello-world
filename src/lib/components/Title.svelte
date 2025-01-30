553bc661f14030bec63dae019d223193f053d71a
adoestreito <adestreito@fastly.com>
2025-01-30 10:20:13 +0100

    Hello world using svelte reactivity

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,20 @@
+<script>
+	let world = $state('world');
+
+	function onclick(event) {
+		world = 'svelte';
+	}
+</script>
+
+<h1>Hello <span>{world}</span></h1>
+<button {onclick}>Change!</button>
+
+<style>
+	h1 {
+		font-family: 'Courier New', Courier, monospace;
+	}
+
+	span {
+		color: red;
+	}
+</style>
